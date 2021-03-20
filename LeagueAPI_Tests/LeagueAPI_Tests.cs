081dd968d0fe0ceb190b46fdd8650d226e8f143c
Simeon Dobrudzhanski <simmeonn1@gmail.com>
2021-03-20 12:40:01 +0000

    Added collection speed test

--- LeagueAPI_Tests/LeagueAPI_Tests.cs
+++ LeagueAPI_Tests/LeagueAPI_Tests.cs
@@ -36,5 +36,54 @@ namespace LeagueAPI_Tests
         //    //GetExcelFilesFromChampsFile("D:\\LeagueAPI_Results\\2020-11-15T11-36-32\\champs.txt");
         //    //WriteItemSetJsonFile(File.ReadAllText(@"D:\LeagueAPI_Results\champs_2020-11-15T11-36-32.txt").ToObject<List<Champion>>());
         //}
+
+        [TestMethod]
+        public async Task CollectionTest()
+        {
+            const int itemsToAdd = 1000;
+
+            List<int> list = new();
+            Queue<int> queue = new();
+            HashSet<int> set = new();
+            Stopwatch timer = new();
+
+            timer.Restart();
+            for (int i = 0; i < itemsToAdd; i++) list.Add(i);
+            double listTime = timer.Elapsed.TotalSeconds;
+
+            timer.Restart();
+            for (int i = 0; i < itemsToAdd; i++) queue.Enqueue(i);
+            double queueTime = timer.Elapsed.TotalSeconds;
+            
+            timer.Restart();
+            for (int i = 0; i < itemsToAdd; i++) set.Add(i);
+            double setTime = timer.Elapsed.TotalSeconds;
+
+            timer.Restart();
+            while (list.Count > 0)
+            {
+                list.RemoveAt(0);
+            }
+            double listCleanTime = timer.Elapsed.TotalSeconds;
+
+            timer.Restart();
+            while (queue.Count > 0)
+            {
+                queue.Dequeue();
+            }
+            double queueCleanTime = timer.Elapsed.TotalSeconds;
+
+            timer.Restart();
+            for (int i = 0; i < itemsToAdd; i++) set.Remove(i);
+            double setCleanTime = timer.Elapsed.TotalSeconds;
+
+            Debug.WriteLine($"List - {listTime}{Environment.NewLine}" +
+                            $"Queue - {queueTime}{Environment.NewLine}" +
+                            $"Set - {setTime}{Environment.NewLine}" +
+                            $"ListCleanTime - {listCleanTime}{Environment.NewLine}" +
+                            $"QueueCleantTime - {queueCleanTime}{Environment.NewLine}" +
+                            $"SetCleanTime - {setCleanTime}{Environment.NewLine}"
+                            );
+        }
     }
 }
